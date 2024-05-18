2eec63fd806b90c651177dedcae6878f8cc7a3b6
Harold Granados <haresje@gmail.com>
2024-05-17 23:22:02 -0500

    update

--- config/adminlte.php
+++ config/adminlte.php
@@ -310,11 +310,6 @@ return [
             'type' => 'sidebar-menu-search',
             'text' => 'search',
         ],
-        [
-            'text' => 'blog',
-            'url' => 'admin/blog',
-            //'can' => 'manage-blog',
-        ],
 
         [
             'text' => ' Adm. Eventos',
