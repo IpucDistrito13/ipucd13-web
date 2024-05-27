2d6df39d00d61eeaa7cf5f39732a037949c11505
Harold Granados <haresje@gmail.com>
2024-05-27 11:40:40 -0500

    update

--- app/Models/Cronograma.php
+++ app/Models/Cronograma.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,4 +11,15 @@ class Cronograma extends Model
     use HasFactory;
     protected $fillable = ['title', 'start', 'end', 'lugar', 'descripcion', 'url'];
 
+    // Accesor para el campo 'start'
+    public function getStartAttribute($value)
+    {
+        return Carbon::parse($value)->format('Y-m-d H:i:s');
+    }
+
+    // Accesor para el campo 'end'
+    public function getEndAttribute($value)
+    {
+        return Carbon::parse($value)->format('Y-m-d H:i:s');
+    }
 }
