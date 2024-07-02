b8d6fc992697a7e2a515c45b9f6a575acd73e987
Harold Granados <haresje@gmail.com>
2024-07-02 08:13:08 -0500

    update

--- resources/views/public/podcasts/episodios.blade.php
+++ resources/views/public/podcasts/episodios.blade.php
@@ -122,7 +122,7 @@
 
     <!-- start banner -->
     <section class="ipad-top-space-margin bg-dark-gray cover-background page-title-big-typography"
-        @if ($podcast->imagen_banner) style="background-image: url({{ Storage::url($podcast->imagen_banner) }} )" @endif>
+        style="background-image: url({{ Storage::url($podcast->imagen_banner) }} )">
         <div class="background-position-center-top h-100 w-100 position-absolute left-0px top-0"
             style="background-image: url('{{ asset('images/vertical-line-bg-small.svg') }}')"></div>
         <div id="particles-style-01" class="h-100 position-absolute left-0px top-0 w-100" data-particle="true"
