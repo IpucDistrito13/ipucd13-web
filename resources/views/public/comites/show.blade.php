1af7f559b095fe0d041a006f1362811e203826de
Harold Granados <haresje@gmail.com>
2024-07-02 07:51:10 -0500

    update

--- resources/views/public/comites/show.blade.php
+++ resources/views/public/comites/show.blade.php
@@ -31,7 +31,24 @@
 
     @include('public.layouts.menu')
 
-    
+    <!-- start banner  -->
+    <section class="ipad-top-space-margin bg-dark-gray cover-background page-title-big-typography"
+        style="background-image: url({{ Storage::url($comite->imagen_banner) }} )">
+        <div class="background-position-center-top h-100 w-100 position-absolute left-0px top-0"
+            style="background-image: url('{{ asset('images/vertical-line-bg-small.svg') }}')"></div>
+        <div id="particles-style-01" class="h-100 position-absolute left-0px top-0 w-100" data-particle="true"
+            data-particle-options='{"particles": {"number": {"value": 10,"density": {"enable": true,"value_area": 2000}},"color": {"value": ["#00338D", "#009FDA", "#F0AB00", "#00338D", "#009FDA"]},"shape": {"type": "circle","stroke":{"width":0,"color":"#000000"}},"opacity": {"value": 1,"random": false,"anim": {"enable": false,"speed": 1,"sync": false}},"size": {"value": 8,"random": true,"anim": {"enable": false,"sync": true}},"line_linked":{"enable":false,"distance":0,"color":"#ffffff","opacity":1,"width":1},"move": {"enable": true,"speed":1,"direction": "right","random": false,"straight": false}},"interactivity": {"detect_on": "canvas","events": {"onhover": {"enable": false,"mode": "repulse"},"onclick": {"enable": false,"mode": "push"},"resize": true}},"retina_detect": false}'>
+        </div>
+        <div class="container">
+            <div class="row align-items-center extra-small-screen">
+                <div class="col-xl-6 col-lg-7 col-md-8 col-sm-9 position-relative page-title-extra-small"
+                    data-anime='{ "el": "childs", "translateY": [-15, 0], "perspective": [1200,1200], "scale": [1.1, 1], "rotateX": [50, 0], "opacity": [0,1], "duration": 800, "delay": 200, "staggervalue": 300, "easing": "easeOutQuad" }'>
+                    <h2 class="fw-500 m-0 ls-minus-2px text-white alt-font">{{ $comite->nombre }}</h2>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- end banner -->
 
     <!-- start footer -->
     @include('public.layouts.footer')
