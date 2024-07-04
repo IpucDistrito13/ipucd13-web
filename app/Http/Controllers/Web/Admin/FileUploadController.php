15c5df69ac137e3a0b3841f5d87ef1eabc8f3993
Harold Granados <haresje@gmail.com>
2024-07-03 23:24:20 -0500

    update

--- app/Http/Controllers/Web/Admin/FileUploadController.php
+++ app/Http/Controllers/Web/Admin/FileUploadController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Web\Admin;
 
 use App\Http\Controllers\Controller;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Pion\Laravel\ChunkUpload\Handler\HandlerFactory;
@@ -29,7 +30,8 @@ class FileUploadController extends Controller
     }
 
     public function uploadLargeFiles(Request $request)
-    {
+{
+    try {
         $receiver = new FileReceiver('file', $request, HandlerFactory::classFromRequest($request));
 
         if (!$receiver->isUploaded()) {
@@ -41,7 +43,7 @@ class FileUploadController extends Controller
         if ($fileReceived->isFinished()) { // file uploading is complete / all chunks are uploaded
             $file = $fileReceived->getFile(); // get file
             $extension = $file->getClientOriginalExtension();
-            $fileName = str_replace('.' . $extension, '', $file->getClientOriginalName()); //file name without extenstion
+            $fileName = str_replace('.' . $extension, '', $file->getClientOriginalName()); //file name without extension
             $fileName .= '_' . md5(time()) . '.' . $extension; // a unique file name
 
             $ubicacion = 'public/podcasts/episodios'; // Define the storage path
@@ -52,18 +54,23 @@ class FileUploadController extends Controller
             // delete chunked file
             unlink($file->getPathname());
 
-            //return $path;
             return [
                 'path' => asset('storage/' . str_replace('public/', '', $path)),
                 'filename' => $fileName
             ];
         }
 
-        // otherwise return percentage informatoin
+        // otherwise return percentage information
         $handler = $fileReceived->handler();
         return [
             'done' => $handler->getPercentageDone(),
             'status' => true
         ];
+
+    } catch (Exception $e) {
+        // handle exception and return error response
+        return response()->json(['error' => 'Ocurrió un error durante la carga del archivo: ' . $e->getMessage()], 500);
     }
+}
+
 }
