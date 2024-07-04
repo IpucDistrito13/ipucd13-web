6e140435901d60c71e47f45863d64eb55091d77b
Harold Granados <haresje@gmail.com>
2024-07-04 01:18:17 -0500

    update

--- app/Http/Controllers/Web/Admin/FileUploadController.php
+++ app/Http/Controllers/Web/Admin/FileUploadController.php
@@ -54,7 +54,7 @@ public function uploadLargeFiles(Request $request)
             $storagePath = asset('storage/podcasts/episodios/' . $fileName);
         } else {
             //$storagePath = env('AWS_URL') . '/' . env('AWS_BUCKET') . '/' . $ubicacion;
-            $storagePath = env('AWS_BUCKET') . '/' . env('AWS_ENDPOINT') . '/' . $ubicacion;
+            $storagePath =  'https://ipucd13.nyc3.digitaloceanspaces.com/' . $ubicacion;
         }
 
         return [
