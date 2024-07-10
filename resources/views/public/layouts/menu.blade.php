91539e4b8ba712e96292589c22cb15d540faaa5b
Harold Granados <haresje@gmail.com>
2024-07-10 17:33:02 -0500

    update

--- resources/views/public/layouts/menu.blade.php
+++ resources/views/public/layouts/menu.blade.php
@@ -2,15 +2,14 @@
 
 <style>
     @font-face {
-    font-family: 'MyriadProBold';
-    src: url('{{ asset('fonts/myriadpro_bold.otf') }}') format('opentype');
+        font-family: 'MyriadProBold';
+        src: url('{{ asset('fonts/myriadpro_bold.otf') }}') format('opentype');
+    }
 
-}
-
-.navbar-nav .nav-link {
-    font-family: 'MyriadProBold', sans-serif;
-}
-
+    .navbar-nav .nav-link,
+    .dropdown-menu .dropdown-item {
+        font-family: 'MyriadProBold', sans-serif;
+    }
 </style>
 
 <header>
@@ -19,17 +18,13 @@
         <div class="container-fluid">
             <div class="col-auto col-lg-2 me-lg-0 me-auto">
                 <a class="navbar-brand" href="{{ route('inicio.index') }}">
-                    <img src="{{ asset('img/logo-colors.png') }}" data-at2x="{{ asset('img/logo-colors@2x.png') }}"
-                        alt="" class="default-logo">
-                    <img src="{{ asset('img/logo-colors.png') }}" data-at2x="{{ asset('img/logo-colors@2x.png') }}"
-                        alt="" class="alt-logo">
-                    <img src="{{ asset('img/logo-colors.png') }}" data-at2x="{{ asset('img/logo-colors@2x.png') }}"
-                        alt="" class="mobile-logo">
+                    <img src="{{ asset('img/logo-colors.png') }}" data-at2x="{{ asset('img/logo-colors@2x.png') }}" alt="" class="default-logo">
+                    <img src="{{ asset('img/logo-colors.png') }}" data-at2x="{{ asset('img/logo-colors@2x.png') }}" alt="" class="alt-logo">
+                    <img src="{{ asset('img/logo-colors.png') }}" data-at2x="{{ asset('img/logo-colors@2x.png') }}" alt="" class="mobile-logo">
                 </a>
             </div>
             <div class="col-auto menu-order position-static">
-                <button class="navbar-toggler float-start" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarNav" aria-controls="navbarNav" aria-label="Toggle navigation">
+                <button class="navbar-toggler float-start" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-label="Toggle navigation">
                     <span class="navbar-toggler-line"></span>
                     <span class="navbar-toggler-line"></span>
                     <span class="navbar-toggler-line"></span>
@@ -40,52 +35,32 @@
                         <li class="nav-item"><a href="{{ route('inicio.index') }}" class="nav-link">Inicio</a></li>
                         <li class="nav-item dropdown dropdown-with-icon">
                             <a href="#" class="nav-link">Calendario</a>
-                            <i class="fa-solid fa-angle-down dropdown-toggle" id="navbarDropdownMenuLink" role="button"
-                                data-bs-toggle="dropdown" aria-expanded="false"></i>
+                            <i class="fa-solid fa-angle-down dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false"></i>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                <li>
-                                    <a href="{{ route('public.eventos.index') }}"><i class="bi bi-laptop"></i>
-                                        <div class="submenu-icon-content">
-                                            <span>Eventos</span>
-                                        </div>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="{{ route('public.cronogramas.index') }}"><i class="bi bi-briefcase"></i>
-                                        <div class="submenu-icon-content">
-                                            <span>Cronograma distrital</span>
-                                        </div>
-                                    </a>
-                                </li>
+                                <li><a href="{{ route('public.eventos.index') }}" class="dropdown-item"><i class="bi bi-laptop"></i><div class="submenu-icon-content"><span>Eventos</span></div></a></li>
+                                <li><a href="{{ route('public.cronogramas.index') }}" class="dropdown-item"><i class="bi bi-briefcase"></i><div class="submenu-icon-content"><span>Cronograma distrital</span></div></a></li>
                             </ul>
                         </li>
                         <li class="nav-item dropdown simple-dropdown">
                             <a href="#" class="nav-link">Comités</a>
-                            <i class="fa-solid fa-angle-down dropdown-toggle" id="navbarDropdownMenuLink5"
-                                role="button" data-bs-toggle="dropdown" aria-expanded="false"></i>
+                            <i class="fa-solid fa-angle-down dropdown-toggle" id="navbarDropdownMenuLink5" role="button" data-bs-toggle="dropdown" aria-expanded="false"></i>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink5">
                                 @foreach ($comites as $comite)
-                                    <li class="dropdown"><a
-                                            href="{{ route('comite.show', $comite) }}">{{ $comite->nombre }}</a></li>
+                                    <li class="dropdown"><a href="{{ route('comite.show', $comite) }}" class="dropdown-item">{{ $comite->nombre }}</a></li>
                                 @endforeach
                             </ul>
                         </li>
                         <li class="nav-item dropdown simple-dropdown">
                             <a href="#" class="nav-link">Zona D13</a>
-                            <i class="fa-solid fa-angle-down dropdown-toggle" id="navbarDropdownMenuLink5"
-                                role="button" data-bs-toggle="dropdown" aria-expanded="false"></i>
+                            <i class="fa-solid fa-angle-down dropdown-toggle" id="navbarDropdownMenuLink5" role="button" data-bs-toggle="dropdown" aria-expanded="false"></i>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink5">
-                                <li class="dropdown">
-                                    <a href="{{ route('public.publicaciones.index') }}">Publicaciones</a>
-                                </li>
-                                <li class="dropdown"><a href="{{ route('public.series.index') }}">Series</a></li>
-                                <li class="dropdown"><a href="{{ route('public.podcasts.index') }}">Podcasts</a></li>
+                                <li class="dropdown"><a href="{{ route('public.publicaciones.index') }}" class="dropdown-item">Publicaciones</a></li>
+                                <li class="dropdown"><a href="{{ route('public.series.index') }}" class="dropdown-item">Series</a></li>
+                                <li class="dropdown"><a href="{{ route('public.podcasts.index') }}" class="dropdown-item">Podcasts</a></li>
                             </ul>
                         </li>
-                        <li class="nav-item"><a href="{{ route('public.descargables.index') }}"
-                                class="nav-link">Descargables</a></li>
-                        <li class="nav-item"><a href="{{ route('public.contacto.index') }}"
-                                class="nav-link">Contacto</a></li>
+                        <li class="nav-item"><a href="{{ route('public.descargables.index') }}" class="nav-link">Descargables</a></li>
+                        <li class="nav-item"><a href="{{ route('public.contacto.index') }}" class="nav-link">Contacto</a></li>
                         <li class="nav-item"><a href="{{ route('login') }}" class="nav-link">Iniciar sesión</a></li>
                     </ul>
                 </div>
