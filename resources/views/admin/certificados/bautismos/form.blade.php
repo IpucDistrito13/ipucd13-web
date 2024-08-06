12dcfe917fd21d2dcd65e36b4fc82f6a37084912
Harold Granados <haresje@gmail.com>
2024-08-06 16:42:48 -0500

    update

--- resources/views/admin/certificados/bautismos/form.blade.php
+++ resources/views/admin/certificados/bautismos/form.blade.php
@@ -5,6 +5,7 @@
         <div class="form-group">
             <label>Nombre y apellidos *</label>
             <input type="text" class="form-control" id="nombre" name="nombre"
+            placeholder="Escriba nombre y apellidos del bautisado"
                 value="{{ old('nombre') }}" >
             @error('nombre')
                 <div class="text-danger">{{ $message }}</div>
@@ -16,7 +17,8 @@
         <!-- text input -->
         <div class="form-group">
             <label>Municipio *</label>
-            <input type="text" class="form-control" id="municipio" name="municipio" 
+            <input type="text" class="form-control" id="municipio" name="municipio"
+            placeholder="Escriba el municipio"
                 value="{{ old('municipio') }}">
             @error('municipio')
                 <div class="text-danger">{{ $message }}</div>
@@ -28,6 +30,7 @@
         <div class="form-group">
             <label for="start">Fecha *</label>
             <input type="date" class="form-control" id="start" name="start" placeholder="Fecha inicio"
+            placeholder="Seleccione la fecha del bautismo"
                 value="{{ old('start') }}">
             @error('start')
                 <div class="text-danger">{{ $message }}</div>
