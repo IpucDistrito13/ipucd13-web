fc08b3c8a52f910fdb7e9d2b29ab1f388dd57f72
Harold Granados <haresje@gmail.com>
2024-08-16 03:05:03 -0500

    update

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -5,7 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-use Vendor\Permission\Models\Permission as OriginalPermission;
+//use Vendor\Permission\Models\Permission as OriginalPermission;
+use Spatie\Permission\Models\Permission as OriginalPermission;
 
 
 class Permission extends OriginalPermission
