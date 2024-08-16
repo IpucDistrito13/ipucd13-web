b6774f9607cbd48dba19fa089f29faceaa50684d
Harold Granados <haresje@gmail.com>
2024-08-16 16:53:09 -0500

    Api v2 listar podcast

---
+++ app/Http/Controllers/Api/V2/PodcastController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\Api\V2;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\PodcastCollection;
+use App\Models\GenerarKeyApi;
+use App\Models\Podcast;
+use Illuminate\Http\Request;
+
+class PodcastController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        //return 'Podcast v2';
+        // Obtener la clave API del parámetro
+        $apiKey = $request->query('api_key');
+
+        $apiKeyExists = GenerarKeyApi::ValidarKeyApi($apiKey)->exists();
+
+        // Si la clave API no existe, devolver un mensaje de error con el código de estado 401
+        if (!$apiKeyExists) {
+            return response()->json([
+                'error' => 'No autorizado',
+                'message' => 'La clave API proporcionada no es válida.'
+            ], 401);
+        }
+
+        // Obtener los parámetros limit y offset de la URL
+        $limit = $request->query('limit', 10);
+        $offset = $request->query('offset', 0);
+
+        // Obtener los comités con los parámetros de limit y offset
+        $comites = Podcast::where('estado', 'Publicado')->orderBy('id', 'asc')
+            ->offset($offset)
+            ->limit($limit)
+            ->get();
+
+        return new PodcastCollection($comites);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api_v2.php
+++ routes/api_v2.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Controllers\Api\V2\ComiteController;
+use App\Http\Controllers\Api\V2\PodcastController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('comites', [ComiteController::class, 'index']);
+Route::get('podcasts', [PodcastController::class, 'index']);
+
 
 Route::middleware(['auth:sanctum'])->group(function() {
 
