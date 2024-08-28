1d283810f8e3e4c45a64609d39aa953dd392f968
Harold Granados <haresje@gmail.com>
2024-08-28 13:55:17 -0500

    update

--- app/Http/Controllers/Api/V2/VideoController.php
+++ app/Http/Controllers/Api/V2/VideoController.php
@@ -3,8 +3,7 @@
 namespace App\Http\Controllers\Api\V2;
 
 use App\Http\Controllers\Controller;
-//use App\Http\Resources\V2\collection\VideoCollection;
-use App\http\Resources\V2\Collection\VideoCollection;
+use App\Http\Resources\V2\Collection\VideoCollection;
 use App\Models\Video;
 use Illuminate\Http\Request;
 
