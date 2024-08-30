e9c4aa75ee041823ddbe5322e7ebad5c1b8f307f
Harold Granados <haresje@gmail.com>
2024-08-30 15:00:37 -0500

    Se actualizo estructura para el login api

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 class LoginController extends Controller
 {
+    /*
     public function login(LoginRequest $request)
     {
         // Verificar si el correo electrónico existe en la base de datos
@@ -57,6 +58,48 @@ class LoginController extends Controller
                     ],
                     'token' => $user->createToken($request->device_name)->plainTextToken,
                 ]
+            ]);
+        }
+
+        // Si la autenticación falla debido a una contraseña incorrecta, devolver mensaje de contraseña incorrecta
+        return response()->json([
+            'message' => 'Contraseña incorrecta.'
+        ], Response::HTTP_UNAUTHORIZED); //401
+    }*/
+
+    public function login(LoginRequest $request)
+    {
+        // Verificar si el correo electrónico existe en la base de datos
+        $user = User::where('email', $request->email)->with('roles')->first();
+
+
+        if (!$user) {
+            // El correo electrónico no existe en la base de datos
+            return response()->json([
+                'message' => 'Correo electrónico no encontrado.'
+            ], Response::HTTP_FOUND); //404
+        }
+
+        // Intentar autenticar al usuario
+        if (Auth::attempt($request->only('email', 'password'))) {
+            // Verificar si el estado del usuario es inactivo
+            if ($user->estado === 'Inactivo') {
+                // Si el usuario está inactivo, devolver un mensaje indicando el estado
+                return response()->json([
+                    'message' => 'Usuario inactivo. Por favor, contacte al administrador.'
+                ], Response::HTTP_FORBIDDEN); //403
+            }
+
+            // Si la autenticación es exitosa y el usuario no está inactivo, continuar con la respuesta
+            return response()->json([
+
+                    'id' => $user->id,
+                    'nombre' => $user->nombre,
+                    'apellidos' => $user->apellidos,
+                    'email' => $user->email,
+                    'isActivo' => true,
+                    'roles' => $user->roles->pluck('name'),
+                    'token' => $user->createToken($request->device_name)->plainTextToken,
             ]);
         }
 
