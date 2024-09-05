3d879b756f4f1aa2edbf96887c78b695b27f6a2f
Harold Granados <haresje@gmail.com>
2024-09-05 15:52:44 -0500

    update

--- app/Http/Resources/CronogramaCollection.php
+++ app/Http/Resources/CronogramaCollection.php
@@ -1,9 +1,9 @@
 <?php
-
 namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceCollection;
+use Carbon\Carbon;
 
 class CronogramaCollection extends ResourceCollection
 {
@@ -15,16 +15,28 @@ class CronogramaCollection extends ResourceCollection
     public function toArray(Request $request): array
     {
         return $this->collection->map(function ($cronograma) {
+            // Parsear las fechas usando Carbon
+            $start = Carbon::parse($cronograma->start);
+            $end = Carbon::parse($cronograma->end);
+
+            // Formatear dependiendo de la hora
+            $formattedStart = $start->format('H:i:s') === '01:00:00' 
+                                ? $start->format('Y-m-d') 
+                                : $start->format('Y-m-d H:i:s');
+                                
+            $formattedEnd = $end->format('H:i:s') === '23:59:00' 
+                                ? $end->format('Y-m-d') 
+                                : $end->format('Y-m-d H:i:s');
+
             return [
                 'id' => $cronograma->id,
                 'nombre' => $cronograma->title,
-                'start' => $cronograma->start,
-                'end' => $cronograma->end,
+                'start' => $formattedStart,   // Formateado según la condición
+                'end' => $formattedEnd,       // Formateado según la condición
                 'lugar' => $cronograma->lugar,
                 'descripcion' => $cronograma->descripcion,
                 'url' => $cronograma->url,            
             ];
         })->toArray();
-        
     }
 }
