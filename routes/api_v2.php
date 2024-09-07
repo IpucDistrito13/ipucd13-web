2af756ef66e7e456e4074bd7a8024d2531551285
Harold Granados <haresje@gmail.com>
2024-09-06 21:57:04 -0500

    update

--- routes/api_v2.php
+++ routes/api_v2.php
@@ -41,11 +41,12 @@ Route::get('congregaciones', [CongregacionController::class, 'index']);
 Route::get('usuarios', [UsuarioController::class, 'getListUsuario']);
 Route::get('usuarios/pastores', [UsuarioController::class, 'getListUsuarioPastor']);
 Route::get('usuarios/lideres', [UsuarioController::class, 'getListUsuarioLider']);
+Route::get('usuario/perfil/{uuid}', [UsuarioController::class, 'show']);
+Route::get('ipucenlinea', [IpucenLineaController::class, 'index']);
+Route::get('galeria/privada/{uuid}', [GaleriaController::class, 'showGaleriaPrivadaUsuario']);
+Route::get('galeria/publica/{uuid}', [GaleriaController::class, 'showGaleriaPublicaUsuario']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
 
-    Route::get('usuario/perfil/{uuid}', [UsuarioController::class, 'show']);
-    Route::get('ipucenlinea', [IpucenLineaController::class, 'index']);
-    Route::get('galeria/privada/{uuid}', [GaleriaController::class, 'showGaleriaPrivadaUsuario']);
-    Route::get('galeria/publica/{uuid}', [GaleriaController::class, 'showGaleriaPublicaUsuario']);
+
 });
