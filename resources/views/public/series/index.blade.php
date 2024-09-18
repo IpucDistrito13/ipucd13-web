5b2647f6d8b13bab339e58d9daf0e937ff3766e7
Harold Granados <haresje@gmail.com>
2024-09-18 00:14:29 -0500

    actualizacion de banners

--- resources/views/public/series/index.blade.php
+++ resources/views/public/series/index.blade.php
@@ -9,30 +9,72 @@
         <meta name="viewport" content="width=device-width,initial-scale=1.0" />
         <meta name="description" content="{{ $metaData['description']}}">
         <meta name="robots" content="noindex">
-        <!-- favicon icon -->
-        @include('public.layouts.iconos')
-    </head>
-    <body class="bg-very-light-gray" data-mobile-nav-style="classic">
-        <!-- start header -->
-        @include('public.layouts.menu')
-        <!-- end header -->
+
+    <!-- favicon icon -->
+    @include('public.layouts.iconos')
+
+        <style>
+        .podcast-row:hover {
+            box-shadow: 0 0 10px #00338d;
+            /* Color: #00338d */
+        }
+
+        .banner-section {
+            width: 100%;
+            height: 0;
+            padding-top: 20.00%;
+            /* (500 / 1920) * 100 */
+            background-size: contain;
+            background-position: center;
+            background-repeat: no-repeat;
+        }
+
+        @media (max-width: 768px) {
+            .banner-section {
+                padding-top: 26.04%;
+                /* Mantiene la misma proporción */
+            }
+        }
+
+        @media (max-width: 480px) {
+            .banner-section {
+                padding-top: 15.04%;
+                /* Mantiene la misma proporción */
+            }
+        }
+    </style>
+
+</head>
+
+<body data-mobile-nav-style="full-screen-menu" data-mobile-nav-bg-color="#00338D" class="custom-cursor">
+    <!-- start cursor -->
+    <div class="cursor-page-inner">
+        <div class="circle-cursor circle-cursor-inner"></div>
+        <div class="circle-cursor circle-cursor-outer"></div>
+    </div>
+    <!-- end cursor -->
+
+    <!-- start header -->
+    @include('public.layouts.menu')
+    <!-- end header -->
 
 <!-- start banner -->
-<section class="ipad-top-space-margin bg-dark-gray cover-background page-title-big-typography"
-style="background-image: url(https://via.placeholder.com/1920x560)">
-<div class="background-position-center-top h-100 w-100 position-absolute left-0px top-0"
-    style="background-image: url('images/vertical-line-bg-small.svg')"></div>
-<div id="particles-style-01" class="h-100 position-absolute left-0px top-0 w-100" data-particle="true"
-    data-particle-options='{"particles": {"number": {"value": 50,"density": {"enable": true,"value_area": 2000}},"color": {"value": ["#00338D", "#009FDA", "#F0AB00"]},"shape": {"type": "circle","stroke":{"width":0,"color":"#000000"}},"opacity": {"value": 1,"random": false,"anim": {"enable": false,"speed": 1,"sync": false}},"size": {"value": 8,"random": true,"anim": {"enable": false,"sync": true}},"line_linked":{"enable":false,"distance":0,"color":"#ffffff","opacity":1,"width":1},"move": {"enable": true,"speed":1,"direction": "right","random": false,"straight": false}},"interactivity": {"detect_on": "canvas","events": {"onhover": {"enable": false,"mode": "repulse"},"onclick": {"enable": false,"mode": "push"},"resize": true}},"retina_detect": false}'>
-</div>
-<div class="container">
-    <div class="row align-items-center extra-small-screen">
-        <div class="col-xl-6 col-lg-7 col-md-8 col-sm-9 position-relative page-title-extra-small"
-            data-anime='{ "el": "childs", "translateY": [-15, 0], "perspective": [1200,1200], "scale": [1.1, 1], "rotateX": [50, 0], "opacity": [0,1], "duration": 800, "delay": 200, "staggervalue": 300, "easing": "easeOutQuad" }'>
-            <h2 class="fw-500 m-0 ls-minus-2px text-white alt-font">Series</h2>
+<section class="ipad-top-space-margin bg-dark-gray cover-background page-title-big-typography banner-section"
+    style="background-image: url('https://ipucd13.nyc3.cdn.digitaloceanspaces.com/public/banners_internos/series_banner2.webp');" >
+    <div class="background-position-center-top h-100 w-100 position-absolute left-0px top-0"
+        style="background-image: url('images/vertical-line-bg-small.svg');">
+    </div>
+    <div id="particles-style-01" class="h-100 position-absolute left-0px top-0 w-100" data-particle="true"
+        data-particle-options='{"particles": {"number": {"value": 50,"density": {"enable": true,"value_area": 2000}},"color": {"value": ["#00338D", "#009FDA", "#F0AB00"]},"shape": {"type": "circle","stroke":{"width":0,"color":"#000000"}},"opacity": {"value": 1,"random": false,"anim": {"enable": false,"speed": 1,"sync": false}},"size": {"value": 8,"random": true,"anim": {"enable": false,"sync": true}},"line_linked":{"enable":false,"distance":0,"color":"#ffffff","opacity":1,"width":1},"move": {"enable": true,"speed":1,"direction": "right","random": false,"straight": false}},"interactivity": {"detect_on": "canvas","events": {"onhover": {"enable": false,"mode": "repulse"},"onclick": {"enable": false,"mode": "push"},"resize": true}},"retina_detect": false}'>
+    </div>
+    <div class="container h-100 position-relative">
+        <div class="row align-items-center h-100 extra-small-screen">
+            <div class="col-xl-6 col-lg-7 col-md-8 col-sm-9 position-relative page-title-extra-small"
+                data-anime='{ "el": "childs", "translateY": [-15, 0], "perspective": [1200,1200], "scale": [1.1, 1], "rotateX": [50, 0], "opacity": [0,1], "duration": 800, "delay": 200, "staggervalue": 300, "easing": "easeOutQuad" }'>
+                <h2 class="fw-500 m-0 ls-minus-2px text-white alt-font"></h2>
+            </div>
         </div>
     </div>
-</div>
 </section>
 <!-- end banner -->
 
