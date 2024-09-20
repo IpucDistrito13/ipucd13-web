788212ceb197ba76a0724cac4b6228f5864f34f6
Harold Granados <haresje@gmail.com>
2024-09-20 00:46:34 -0500

    actualizacion analytics

--- resources/views/public/layouts/analytics.blade.php
+++ resources/views/public/layouts/analytics.blade.php
@@ -1,10 +1,14 @@
+@if (App::environment('production'))
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-G646ETRSJ5"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
 
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-G646ETRSJ5"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
 
-  gtag('config', 'G-G646ETRSJ5');
-</script>
+        gtag('config', 'G-G646ETRSJ5');
+    </script>
+@endif
