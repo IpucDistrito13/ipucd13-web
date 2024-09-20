78d34d00500e842d6d7230bfe7eaa8057cab0744
Harold Granados <haresje@gmail.com>
2024-09-20 01:39:29 -0500

    politicas

--- app/Http/Controllers/Web/Admin/PoliticaController.php
+++ app/Http/Controllers/Web/Admin/PoliticaController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Web\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Comite;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use App\Constants\CacheKeys;
+use App\Models\Congregacion;
+use App\Models\Redes;
 
 class PoliticaController extends Controller
 {
@@ -15,8 +19,46 @@ class PoliticaController extends Controller
     {
         $comitesMenu = Comite::ComiteMenu()->get();
 
+        $comitesMenu = Cache::remember(CacheKeys::PUBLIC_COMITES_MENU, null, function () {
+            return Comite::ComiteMenu()->get();
+        });
+
+        $congregaciones = Cache::remember(CacheKeys::PUBLIC_CONGREGACIONES, null, function () {
+            return Congregacion::all();
+        });
+
+        //return Redes::Activo()->get();
+        $socialData = Cache::remember(CacheKeys::PUBLIC_SOCIAL_DATA, null, function () {
+            $redes_sociales = Redes::Activo()->get();
+            $data = [
+                'links' => ['facebook' => '', 'youtube' => '', 'instagram' => ''],
+                'transmision' => Redes::GetTransmision()->first()
+            ];
+
+            foreach ($redes_sociales as $redSocial) {
+                switch ($redSocial["nombre"]) {
+                    case "Facebook":
+                        $data['links']['facebook'] = $redSocial["url"];
+                        break;
+                    case "YouTube":
+                        $data['links']['youtube'] = $redSocial["url"];
+                        break;
+                    case "Instagram":
+                        $data['links']['instagram'] = $redSocial["url"];
+                        break;
+                }
+            }
+
+            return $data;
+        });
+        
+
         return view('public.politicas.politicas_tratamientodatos',[
             'comites' => $comitesMenu,
+            'transmision' => $socialData['transmision'],
+            'facebook' => $socialData['links']['facebook'],
+            'youtube' => $socialData['links']['youtube'],
+            'instagram' => $socialData['links']['instagram'],
 
         ]);
     }
